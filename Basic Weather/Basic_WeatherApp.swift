8fd6a621cfcfa81804c95fb5ebcfbe5b1f8d3dbd
ahmetbost <ahmetbostanciogluu@gmail.com>
2025-05-05 19:48:47 +0300

    Initial Commit

---
+++ Basic Weather/Basic_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_WeatherApp.swift
+//  Basic Weather
+//
+//  Created by Ahmet Bostancıoğlu on 5.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Basic_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
